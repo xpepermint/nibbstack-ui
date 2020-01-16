32e884bca7fc25e592198441b1437e18b410a40f
David Ličen <davidlicen@gmail.com>
2020-01-16 10:29:23 +0100

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo 'nibbstack.com' > CNAME
 git init
 git add -A
 git commit -m "Deploy page"
-git push -f git@github.com:xpepermint/nibbstack-ui gh-pages
+git push -f git@github.com:xpepermint/nibbstack-ui.git master:gh-pages
 cd -
 echo ''
 echo '-------------------------------------------------------'
